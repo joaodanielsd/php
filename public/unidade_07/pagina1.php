eb693a2291c3b64683ee37ecfacbe2f02118da7c
João Daniel dos Santos Dias <joao_daniel_jd@yahoo.com.br>
2021-10-27 08:07:51 -0300

    Add files via upload

---
+++ public/unidade_07/pagina1.php
@@ -0,0 +1,36 @@
+<?php require_once("../../conexao/conexao.php"); ?>
+<?php
+        session_start();
+
+        $_SESSION["usuario"] = "Mathues"
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP Integração com MySQL</title>
+        
+        <!-- estilo -->
+        <link href="_css/estilo.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php include_once("../_incluir/topo.php"); ?>
+        <?php include_once("../_incluir/funcoes.php"); ?>
+        
+        <main>
+            <?php
+                echo $_SESSION["usuario"];
+
+            ?>
+            <a href="pagina2.php">Pagina 2</a>
+        </main>
+
+        <?php include_once("../_incluir/rodape.php"); ?> 
+    </body>
+</html>
+
+<?php
+    // Fechar conexao
+    mysqli_close($conecta);
+?>
